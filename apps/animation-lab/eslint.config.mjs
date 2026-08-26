53cd5f9e66215e6d565e056d9da0e7ac54b60c62
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-25 23:03:33 -0500

    Ignore generated Storybook output in Animation Lab lint

--- apps/animation-lab/eslint.config.mjs
+++ apps/animation-lab/eslint.config.mjs
@@ -2,6 +2,9 @@ import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
+  {
+    ignores: ['storybook-static/**'],
+  },
   ...nx.configs['flat/react'],
   ...baseConfig,
   {
